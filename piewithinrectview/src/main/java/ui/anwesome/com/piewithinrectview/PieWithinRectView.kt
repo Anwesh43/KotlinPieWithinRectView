a7884aa7e4d950fde38ce262f9ea4520ab606b30
Anwesh43 <anweshthecool0@gmail.com>
2018-03-14 09:39:18 +0530

    adding state class

--- piewithinrectview/src/main/java/ui/anwesome/com/piewithinrectview/PieWithinRectView.kt
+++ piewithinrectview/src/main/java/ui/anwesome/com/piewithinrectview/PieWithinRectView.kt
@@ -19,4 +19,27 @@ class PieWithinRectView(ctx : Context) : View(ctx) {
         }
         return true
     }
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0, var jDir : Int = 1) {
+        val scales : Array<Float> = arrayOf(0f, 0f)
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                prevScale = scales[j] + dir
+                j += jDir
+                if (j == scales.size || j == -1) {
+                    jDir *= -1
+                    j += jDir
+                    dir = 0f
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
+        fun startUpdating(startcb : (Float) -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
