b050f29f343a315fb5720f30c928dd69fabc27ac
Anwesh43 <anweshthecool0@gmail.com>
2018-03-14 09:30:25 +0530

    adding PieWithinRectView

--- piewithinrectview/src/main/java/ui/anwesome/com/piewithinrectview/PieWithinRectView.kt
+++ piewithinrectview/src/main/java/ui/anwesome/com/piewithinrectview/PieWithinRectView.kt
@@ -6,3 +6,17 @@ package ui.anwesome.com.piewithinrectview
 import android.content.*
 import android.graphics.*
 import android.view.*
+class PieWithinRectView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
